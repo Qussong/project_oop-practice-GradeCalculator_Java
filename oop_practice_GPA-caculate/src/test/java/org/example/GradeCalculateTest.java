1e113fb3230649fe3b253202f07c0c6dbec9199c
Gihoon <89381041+Qussong@users.noreply.github.com>
2025-01-05 23:53:35 +0900

    프로젝트 생성 및 업로드
    Grade Calculate 프로젝트 생성 및 업로드

---
+++ oop_practice_GPA-caculate/src/test/java/org/example/GradeCalculateTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 요구사항 :
+ * - 평균학점 계산 방법 = (학점수×교과목 평점)의 합계/수강신청 총학점 수
+ * - MVC패턴(Model-View-Controller) 기반으로 구현한다.
+ * - 일급 컬렉션 사용
+ */
+
+public class GradeCalculateTest {
+
+    @Test
+    void calculateGradeTest() {
+
+    }
+}
